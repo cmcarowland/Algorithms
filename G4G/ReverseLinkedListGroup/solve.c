02d8bc09592ac24cbb21c97d7e543df7385ee335
cmcarowland <82390744+cmcarowland@users.noreply.github.com>
2023-07-21 14:58:50 +0000

    Completed G4G 7/21/23 POTD
    feat: Added ReverseLinkedListGroup project.

---
+++ G4G/ReverseLinkedListGroup/solve.c
@@ -0,0 +1,137 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct node
+{
+    int data;
+    struct node* next;
+    
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+/* Function to print linked list */
+void printList(struct node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+/*
+  Reverse a linked list
+  The input list will have at least one element  
+  Return the node which points to the head of the new LinkedList
+  Node is defined as 
+    struct node
+    {
+        int data;
+        struct node* next;
+    
+        node(int x){
+            data = x;
+            next = NULL;
+        }
+    
+    }*head;
+*/
+
+class Solution
+{
+    public:
+    struct node *reverse (struct node *head, int k)
+    { 
+        stack<struct node*> s;
+        struct node *newHead = nullptr;
+        struct node *current = nullptr;
+
+        while(true)
+        {
+            for(int i = 0; i < k; i++)
+            {
+                s.push(head);  
+                head = head->next;
+                if(head == nullptr) 
+                    break;
+                
+            }
+        
+            while(!s.empty())
+            {
+                if(newHead == nullptr)
+                {
+                    newHead = s.top();
+                    s.pop();
+                    current = newHead;
+                }
+                else
+                {
+                    current->next = s.top();
+                    s.pop();
+                    current = current->next;
+                }
+                
+                current->next = nullptr;
+            }
+
+            if(head == nullptr)
+                return newHead;
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+
+/* Drier program to test above function*/
+int main(void)
+{
+    int t;
+    cin>>t;
+     
+    while(t--)
+    {
+        struct node* head = NULL;
+        struct node* temp = NULL;
+        int n;
+        cin >> n;
+         
+        for(int i=0 ; i<n ; i++)
+        {
+            int value;
+            cin >> value;
+            if(i == 0)
+            {
+                head = new node(value);
+                temp = head;
+            }
+            else
+            {
+                temp->next = new node(value);
+                temp = temp->next;
+            }
+        }
+        
+        int k;
+        cin>>k;
+        
+        Solution ob;
+        head = ob.reverse(head, k);
+        printList(head);
+    }
+     
+    return(0);
+}
+
+
+// } Driver Code Ends
